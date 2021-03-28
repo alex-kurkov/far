8a834258a864bc02c561e20d5c7dca4cfed632fd
Diana Rukavishnikova <psihomatika@gmail.com>
2021-03-28 15:37:16 +0300

    added fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/normalize.css'],
+  css: [
+    '@/assets/normalize.css',
+    '@/assets/main.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/vue-notifications'],
