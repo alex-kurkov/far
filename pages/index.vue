be373305ce3e1dafb1bf74f0dbb280ed81faa243
Anna Kozhevatova <ad.kozhevatova@yandex.ru>
2021-03-30 15:07:42 +0300

    feat: Help

--- pages/index.vue
+++ pages/index.vue
@@ -22,10 +22,11 @@ export default {}
   align-items: flex-start;
   text-align: center;
   flex-wrap: wrap;
+  overflow: hidden;
 }
 
 @media screen and (max-width: 768px) {
-  container {
+  .container {
     flex-direction: column;
   }
 }
