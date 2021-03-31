086c2e84008d3eb3c6be7ea16057e49edf63d2c3
Mikhail Lisus <lisusmikhail@gmail.com>
2021-03-31 02:13:10 -0400

    mobile first: header & about & menu & falling drop refactor

---
+++ components/FallingDrop.vue
@@ -0,0 +1,80 @@
+<template>
+  <transition name="falling-drop" @enter="enter" @after-enter="afterEnter">
+    <icon v-show="isMenuOpen" name="drop" class="falling-drop"></icon>
+  </transition>
+</template>
+
+<script>
+import Icon from '~/components/Icon'
+export default {
+  name: 'FallingDrop',
+  props: ['isMenuOpen'],
+  components: { Icon },
+  data() {
+    return {
+      greyDropTopPosition: 172,
+    }
+  },
+  methods: {
+    enter(el) {
+      el.style.top = '5px'
+      el.style.display = 'block'
+      setTimeout(() => {
+        el.style.top = this.greyDropTopPosition + 'px'
+      }, 0)
+    },
+    afterEnter(el) {
+      el.style.top = this.greyDropTopPosition + 'px'
+      el.style.display = 'none'
+    },
+    handleWindowResize() {
+      if (window.innerWidth >= 769) {
+        this.greyDropTopPosition = 264
+      } else if (window.innerWidth < 769 && window.innerWidth >= 640) {
+        this.greyDropTopPosition = 304
+      } else if (window.innerWidth < 640) {
+        this.greyDropTopPosition = 172
+      }
+    },
+  },
+  mounted() {
+    window.addEventListener('resize', this.handleWindowResize)
+    this.handleWindowResize()
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleWindowResize)
+  },
+}
+</script>
+
+<style scoped>
+.falling-drop {
+  position: absolute;
+  right: 13px;
+  fill: #8c8c8c;
+  width: 22px;
+  height: 40px;
+  z-index: 2;
+  display: none;
+}
+
+.falling-drop-enter-active {
+  transition: top 0.7s ease-in-out;
+}
+
+@media screen and (min-width: 640px) {
+  .falling-drop {
+    right: 22px;
+    width: 34px;
+    height: 60px;
+  }
+}
+
+@media screen and (min-width: 769px) {
+  .falling-drop {
+    right: 17px;
+    width: 34px;
+    height: 60px;
+  }
+}
+</style>
--- components/Hamburger.vue
+++ components/Hamburger.vue
@@ -23,23 +23,21 @@
       </button>
       <icon name="drop" class="hamburger__drop"></icon>
     </div>
-    <transition name="falling-drop" @enter="enter" @after-enter="afterEnter">
-      <icon v-show="isMenuOpen" name="drop" class="falling-drop"></icon>
-    </transition>
+    <FallingDrop :isMenuOpen="isMenuOpen" />
     <Navigation :isMenuOpen="isMenuOpen" />
   </div>
 </template>
 
 <script>
+import FallingDrop from '@/components/FallingDrop'
 import Navigation from '~/components/Navigation'
 import Icon from '~/components/Icon'
 export default {
   name: 'Hamburger',
-  components: { Navigation, Icon },
+  components: { FallingDrop, Navigation, Icon },
   data() {
     return {
       isMenuOpen: false,
-      greyDropTopPosition: 268,
     }
   },
   methods: {
@@ -52,31 +50,6 @@ export default {
       )
       hamburgerIconElement.setAttribute('aria-expanded', `${isOpen}`)
     },
-    enter(el) {
-      el.style.top = '5px'
-      setTimeout(() => {
-        el.style.top = this.greyDropTopPosition + 'px'
-      }, 0)
-    },
-    afterEnter(el) {
-      el.style.top = this.greyDropTopPosition + 'px'
-    },
-    handleWindowResize() {
-      if (window.innerWidth >= 769) {
-        this.greyDropTopPosition = 264
-      } else if (window.innerWidth < 769 && window.innerWidth >= 640) {
-        this.greyDropTopPosition = 304
-      } else if (window.innerWidth < 640) {
-        this.greyDropTopPosition = 172
-      }
-    },
-  },
-  mounted() {
-    window.addEventListener('resize', this.handleWindowResize)
-    this.handleWindowResize()
-  },
-  destroyed() {
-    window.removeEventListener('resize', this.handleWindowResize)
   },
 }
 </script>
@@ -142,19 +115,6 @@ export default {
   z-index: 3;
 }
 
-.falling-drop {
-  position: absolute;
-  right: 13px;
-  fill: #8c8c8c;
-  width: 22px;
-  height: 40px;
-  z-index: 2;
-}
-
-.falling-drop-enter-active {
-  transition: top 0.7s ease-in-out;
-}
-
 @media screen and (min-width: 640px) {
   .hamburger__menu {
     top: 10px;
@@ -180,12 +140,6 @@ export default {
     width: 36px;
     height: 62px;
   }
-
-  .falling-drop {
-    right: 22px;
-    width: 34px;
-    height: 60px;
-  }
 }
 
 @media screen and (min-width: 769px) {
@@ -207,12 +161,6 @@ export default {
     padding: 3px 0 0 0;
     height: 48px;
     width: 159px;
-  }
-
-  .falling-drop {
-    right: 17px;
-    width: 34px;
-    height: 60px;
   }
 }
 
