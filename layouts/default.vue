ddbf567564b65cafc9895a93650b32bf0c85b8bd
alex-kurkov <alexkourkov@yandex.ru>
2021-04-07 19:31:13 +0500

    took Notifications to isolated component

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,37 +4,13 @@
     <Header />
     <Nuxt />
     <Footer />
-    <notifications group="notification" position="right bottom" :max="2">
-      <template slot="body" slot-scope="{ item, close }">
-        <div
-          :class="`notification notification_type_${item.data.type}`"
-          @click="close"
-        >
-          <!--           <div class="icon">
-            <i class="icon-svg" @click="close"></i>
-          </div> -->
-          <div class="notification__content">
-            <h4 class="notification__title" v-html="item.title" />
-            <p class="notification__text" v-html="item.text" />
-            <div
-              v-if="item.data.action"
-              class="notification__btn"
-              v-html="item.data.actionBtnText"
-              @click.prevent="item.data.action"
-            />
-          </div>
-        </div>
-      </template>
-    </notifications>
+    <Notifications />
   </div>
 </template>
 <script>
+import Notifications from '../components/Notifications.vue'
 export default {
-  methods: {
-    clean(group) {
-      this.$notify({ group, clean: true })
-    },
-  },
+  components: { Notifications },
   mounted() {
     this.$notify({
       group: 'notification',
@@ -96,60 +72,5 @@ export default {
   /* max-width: 1280px; */
   width: 100%;
   font-family: Vollkorn, Arial, sans-serif;
-}
-.vue-notification-group {
-  position: fixed;
-  display: block;
-  box-sizing: border-box;
-  width: 320px;
-  z-index: 10000;
-}
-.notification {
-  width: 88%;
-  box-sizing: border-box;
-  min-height: 128px;
-  border-radius: 20px;
-  padding: 20px;
-  margin: 24px;
-  box-shadow: 0 0 4px #000;
-  cursor: pointer;
-}
-.notification_type_news {
-  background: #000;
-  color: #fff;
-}
-.notification_type_info {
-  background: #fff;
-  color: #000;
-}
-
-.notification__content {
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-  align-content: flex-start;
-  justify-items: flex-start;
-}
-.notification__title {
-  font-family: 'Vollkorn', sans-serif;
-  font-size: 24px;
-  font-weight: 700;
-}
-.notification__text {
-  color: #666666;
-  font-family: Roboto, sans-serif;
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 19px;
-  text-align: left;
-}
-.notification__btn {
-  cursor: pointer;
-  display: block;
-  font-family: 'Vollkorn', sans-serif;
-  font-size: 24px;
-  font-weight: 700;
-  color: #ff0000;
-  text-decoration: underline;
 }
 </style>
