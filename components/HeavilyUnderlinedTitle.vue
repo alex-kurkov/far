7836e704d303982325543cabc79a2dff511e4bc6
alex-kurkov <alexkourkov@yandex.ru>
2021-04-11 14:58:36 +0500

    add HeavilyUnderlinedTitle component

---
+++ components/HeavilyUnderlinedTitle.vue
@@ -0,0 +1,67 @@
+<template>
+  <h2 class="">
+    <span 
+    class="word"
+    v-for="(word, index) in words" 
+    :key="index" 
+    >
+      {{ word }}
+      <div class="underline" v-bind:style="{ 'background-color': lineColor, height: height + 'px', bottom: bottomOffcet + 'px' }" />
+    </span>
+  </h2>
+</template>
+
+<script>
+  export default {
+    name: 'HeavilyUnderlinedTitle',
+    props: {
+      title: {
+        type: String,
+        default: '',
+      },
+      lineColor: {
+        type: String,
+        default: '#b33438',
+      },
+      height: {
+        type: String,
+        default: '11',
+      },
+      bottomOffcet: {
+        type: String,
+        default: '4',
+      }
+    },
+    computed: {
+      words() {
+        return this
+          .title
+          .split(' ')
+          .reduce(
+            (acc, word, index, arr) => {
+              acc.push(word);
+              if (index !== arr.length - 1) acc.push(' ');
+              return acc;
+            }, [])
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.word {
+  display: inline;
+  position: relative;
+  white-space: nowrap;
+}
+
+.underline {
+  content: '';
+  width: 100%;
+  position: absolute;
+  left: 0;
+}
+.word:last-of-type .underline {
+  width: 90%;
+}
+</style>
--- components/ReportsPromo.vue
+++ components/ReportsPromo.vue
@@ -1,14 +1,21 @@
 <template>
   <section class="report-promo">
-    <h1>Финансовые и содержательные годовые отчеты</h1>
+    <div class="report-promo__title-wrap"> 
+      <HeavilyUnderlinedTitle 
+      class="report-promo__title" 
+      :title="'Финансовые и содержательные годовые отчеты'" 
+      lineColor="#b33438"
+      height='11'
+      bottomOffcet="4"
+      />
+    </div>
+      <div class="report-promo__logo"></div>
   </section>
 </template>
 
 <script>
-import AllReports from '@/components/AllReports'
 export default {
   name: 'ReportPromo',
-  components: { AllReports },
 }
 </script>
 
@@ -17,5 +24,37 @@ export default {
   height: 150px;
   width: 100%;
   background-color: greenyellow;
+  display: flex;
+  justify-content: space-between;
+}
+.report-promo__title-wrap {
+  width: 50%;
+}
+.report-promo__title {
+  min-height: 104px;
+  color: #313131;
+  font-family: Vollkorn, Arial, sans-serif;
+  font-size: 25px;
+  line-height: 23px;
+  letter-spacing: normal;
+  font-weight: 700;
+  text-align: left;
+  margin: 23px 0 23px 14px;
+  padding: 0;
+}
+
+.report-promo__logo {
+  width: 50%;
+  min-height: 104px;
+  background: black;
+}
+.repor {
+  content: '';
+  height: 11px;
+  width: 100%;
+  position: absolute;
+  background-color: #b33438;
+  top: 14px;
+  left: 0;
 }
 </style>
