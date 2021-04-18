e3b63a200fb444e9ca95c22ce58b480b9835fc2e
alex-kurkov <alexkourkov@yandex.ru>
2021-04-18 12:33:18 +0500

    feat: set localStore cookies

--- store/index.js
+++ store/index.js
@@ -3,6 +3,7 @@ import dictionary from '@/utils/dictionary'
 
 export const state = () => ({
   content: {},
+  cookies: {}
 })
 
 export const getters = {
@@ -13,12 +14,18 @@ export const getters = {
   getLocaleMessage: (state) => (locale) => {
     return dictionary(state.content, locale)
   },
+  getCookiesAcceptanceStatus: (state) => {
+    return state.cookies.accepted;
+  }
 }
 
 export const mutations = {
   SET_CONTENT(state, data) {
     state.content = data
   },
+  SET_COOKIES_ACCEPTANCE(state, bool) {
+    state.cookies.accepted = bool;
+  } 
 }
 
 export const actions = {
