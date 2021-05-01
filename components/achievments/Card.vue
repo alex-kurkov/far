869555056cdb348f647146b09f8d565ea086020e
alex-kurkov <alexkourkov@yandex.ru>
2021-05-01 15:12:41 +0500

    fix digit bg in achievements

--- components/achievments/Card.vue
+++ components/achievments/Card.vue
@@ -55,7 +55,7 @@ export default {
   width: calc(100% - 150px);
   padding: 12px;
   overflow: hidden;
-  background: linear-gradient(to right, transparent 10%, #d5d5d5 11%);
+  background: linear-gradient(to right, transparent 10%, #000 10%);
 }
 .card__stamp-text {
   text-align: center;
